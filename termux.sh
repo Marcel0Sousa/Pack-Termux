373214a3b06d21fd7fdc58856b3361956c37555b
tchelospy <tchelospy@gmail.com>
2019-02-23 20:10:23 -0300

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -118,10 +118,10 @@ echo
 git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh --depth 1
 cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
 chsh -s zsh
-;;
+
 # echo figlet KALI LINUX >> /data/data/com.termux/files/usr/etc/zshrc
 echo
-
+;;
 n)
 echo "Pacote não instalado :-("
 ;;
